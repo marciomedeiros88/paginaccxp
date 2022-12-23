1adbe7ec870aebd558e90641db55af61f1a2a558
Márcio Medeiros <118381472+marciomedeiros88@users.noreply.github.com>
2022-12-22 21:10:06 -0300

    separando countdown de efeito

--- countdown_script.js
+++ countdown_script.js
@@ -26,37 +26,3 @@ function formatacao(tempo){
 }
 countdown();
 setInterval(countdown, 1000)
-
-
-function cresceBotaoQuinta(){
-    let ingQuinta = document.getElementById('quinta').style.width = "145px";
-    ingQuinta = document.getElementById('quinta').style.transition = "1000ms";
-}
-
-function decresceBotaoQuinta(){
-    let ingQuinta = document.getElementById('quinta').style.width = "auto";
-}
-
-function cresceBotaoSexta(){
-    let ingSexta = document.getElementById('sexta').style.width = "145px";
-}
-
-function decresceBotaoSexta(){
-    let ingSexta = document.getElementById('sexta').style.width = "auto";
-}
-
-function cresceBotaoSabado(){
-    let ingSabado = document.getElementById('sabado').style.width = "145px";
-}
-
-function decresceBotaoSabado(){
-    let ingSabado = document.getElementById('sabado').style.width = "auto";
-}
-
-function cresceBotaoDomingo(){
-    let ingDomingo = document.getElementById('domingo').style.width = "145px";
-}
-
-function decresceBotaoDomingo(){
-    let ingDomingo = document.getElementById('domingo').style.width = "auto";
-}
