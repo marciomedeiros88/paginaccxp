00cf88d1e64b62817e05858e611f805c3a88a345
Márcio Medeiros <118381472+marciomedeiros88@users.noreply.github.com>
2023-01-02 09:58:40 -0300

    adicionando seletor de ingressos

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 
+const ingressos = [];
 
 function cresceBotaoQuinta(){
     let ingQuinta = document.getElementById('quinta').style.width = "145px";
@@ -31,4 +32,15 @@ function cresceBotaoDomingo(){
 
 function decresceBotaoDomingo(){
     let ingDomingo = document.getElementById('domingo').style.width = "auto";
+}
+
+function selecionarIngresso(selector){
+    var element = document.querySelector(selector);
+    element.classList.toggle("card-selected");
+    if(ingressos.includes(selector)) ingressos.pop(selector);
+    else ingressos.push(selector);
+}
+
+function mostrarIngressos(){
+    if(ingressos.length > 0) alert("Ingressos Selecionados:" + ingressos);
 }
